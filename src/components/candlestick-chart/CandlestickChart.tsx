5ee405c3e578d48ca71b25b70ae5942b1ca95005
Igor Sidorov <codeBurger770@gmail.com>
2022-04-24 22:15:38 +0300

    Различные правки

--- src/components/candlestick-chart/CandlestickChart.tsx
+++ src/components/candlestick-chart/CandlestickChart.tsx
@@ -19,47 +19,52 @@ export function CandlestickChart(props: ICandlestickChartProps) {
     const [xClient, setXClient] = useState(0);
     const [xOffset, setXOffset] = useState(0);
 
-    const { candlesticksWithX, xMin } = useMemo(() => {
-        const candlesticksWithX = [...props.data].reverse().map((i, index) => ({
+    const { dataWithX, xMin } = useMemo(() => {
+        const dataWithX = [...props.data].reverse().map((i, index) => ({
             ...i,
             x: 875 - index * 20,
         })).reverse();
         return {
-            candlesticksWithX,
-            xMin: candlesticksWithX[0].x,
+            dataWithX,
+            xMin: dataWithX.length ? dataWithX[0].x : 0,
         };
     }, [props.data]);
 
-    const { candlesticksWithXOffset, valueMin, valueMax } = useMemo(() => {
-        const candlesticksWithXOffset = candlesticksWithX.map(i => ({
+    const { dataWithXOffset, valueMin, valueMax } = useMemo(() => {
+        const dataWithXOffset = dataWithX.map(i => ({
             ...i,
             x: i.x - xOffset,
         }));
-        const values = candlesticksWithXOffset
+        const values = dataWithXOffset
             .filter(i => i.x >= 125 && i.x <= 875)
             .map(i => [i.high, i.low, i.open, i.close])
             .flat();
         return {
-            candlesticksWithXOffset,
+            dataWithXOffset,
             valueMin: Math.min(...values),
             valueMax: Math.max(...values),
         };
-    }, [candlesticksWithX, xOffset]);
+    }, [dataWithX, xOffset]);
 
     const yAxis = useMemo(() => {
-        const yAxis = [];
-        const { close } = candlesticksWithXOffset[candlesticksWithXOffset.length - 1];
+        const yAxis: JSX.Element[] = [];
+
+        if (!dataWithXOffset.length) {
+            return yAxis;
+        }
+
+        const { close } = dataWithXOffset[dataWithXOffset.length - 1];
 
         if (close < valueMin) {
-            yAxis.push(<text key="yAxis-close" x="990" y="425" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
+            yAxis.push(<text key="yAxis-close" x="995" y="425" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
         } else if (close > valueMax) {
-            yAxis.push(<text key="yAxis-close" x="990" y="25" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
+            yAxis.push(<text key="yAxis-close" x="995" y="25" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
         } else {
-            const y = interpolate(close, [valueMin, valueMax], [425, 25]);
+            const y = interpolate(close, [valueMin, valueMax], [425, 25]) || 25;
             yAxis.push(
                 <React.Fragment key="yAxis-close">
                     <line x1="100" y1={y} x2="900" y2={y} stroke="#F26126" strokeDasharray="10 5" />
-                    <text x="990" y={y} fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>
+                    <text x="995" y={y} fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>
                 </React.Fragment>
             );
         }
@@ -72,26 +77,28 @@ export function CandlestickChart(props: ICandlestickChartProps) {
             yAxis.push(
                 <React.Fragment key={`yAxis-${value}`}>
                     <line x1="100" y1={y} x2="900" y2={y} stroke="#F7F7F8" />
-                    <text x="10" y={y} fill="#9393A1" alignmentBaseline="middle">{value} &#8381;</text>
+                    <text x="5" y={y} fill="#9393A1" alignmentBaseline="middle">{value} &#8381;</text>
                 </React.Fragment>
             );
         }
 
         return yAxis;
-    }, [candlesticksWithXOffset, valueMin, valueMax]);
+    }, [dataWithXOffset, valueMin, valueMax]);
 
     const { xAxis, candlesticks } = useMemo(() => {
         const xAxis: JSX.Element[] = [];
         const candlesticks: JSX.Element[] = [];
         let datePrev = '';
+        let xPrev = dataWithXOffset.length ? dataWithXOffset[0].x : 0;
 
-        candlesticksWithXOffset.forEach(i => {
+        dataWithXOffset.forEach(i => {
             const date = new Date(i.dateTime).toLocaleDateString();
 
             if (datePrev !== date) {
                 datePrev = date;
 
-                if (i.x >= 125 && i.x <= 875) {
+                if (i.x >= 125 && i.x <= 875 && (i.x - xPrev >= 100 || i.x - xPrev === 0)) {
+                    xPrev = i.x;
                     xAxis.push(
                         <React.Fragment key={`xAxis-${date}`}>
                             <line x1={i.x} y1="0" x2={i.x} y2="450" stroke="#F7F7F8" />
@@ -120,7 +127,7 @@ export function CandlestickChart(props: ICandlestickChartProps) {
             xAxis,
             candlesticks,
         };
-    }, [candlesticksWithXOffset]);
+    }, [dataWithXOffset]);
 
     const tooltip = useMemo(() => {
         if (!ref.current || isMoving || !xClient) {
@@ -129,43 +136,43 @@ export function CandlestickChart(props: ICandlestickChartProps) {
 
         const boundingClientRect = ref.current.getBoundingClientRect();
         let x = (xClient - boundingClientRect.x) / boundingClientRect.width * 1000;
-        let candlestickHovering: any;
+        let itemHovering: any;
         let intervalPrev = 1000;
-        candlesticksWithXOffset.forEach(i => {
+        dataWithXOffset.forEach(i => {
             const interval = Math.abs(i.x - x);
 
             if (interval <= 10 && interval < intervalPrev) {
                 intervalPrev = interval;
-                candlestickHovering = i;
+                itemHovering = i;
             }
         });
 
-        if (!candlestickHovering || (candlestickHovering.x < 125 || candlestickHovering.x > 875)) {
+        if (!itemHovering || (itemHovering.x < 125 || itemHovering.x > 875)) {
             return null;
         }
 
-        x = candlestickHovering.x >= 500 ? candlestickHovering.x - 320 : candlestickHovering.x + 20;
-        const dateTime = new Date(candlestickHovering.dateTime);
+        x = itemHovering.x >= 500 ? itemHovering.x - 320 : itemHovering.x + 20;
+        const dateTime = new Date(itemHovering.dateTime);
 
         return (
             <>
-                <rect x={candlestickHovering.x - 10} y="0" width={20} height="450" fill="#9393A1" fillOpacity="0.5" />
+                <rect x={itemHovering.x - 10} y="0" width={20} height="450" fill="#9393A1" fillOpacity="0.5" />
                 <rect x={x} y="160" width={300} height="130" fill="#FFFFFF" stroke="#9393A1" />
                 <text x={x + 10} y="175" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Открытие</text>
-                <text x={x + 290} y="175" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.open} &#8381;</text>
+                <text x={x + 290} y="175" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.open} &#8381;</text>
                 <text x={x + 10} y="195" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Закрытие</text>
-                <text x={x + 290} y="195" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.close} &#8381;</text>
+                <text x={x + 290} y="195" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.close} &#8381;</text>
                 <text x={x + 10} y="215" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Максимум</text>
-                <text x={x + 290} y="215" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.high} &#8381;</text>
+                <text x={x + 290} y="215" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.high} &#8381;</text>
                 <text x={x + 10} y="235" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Минимум</text>
-                <text x={x + 290} y="235" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.low} &#8381;</text>
+                <text x={x + 290} y="235" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.low} &#8381;</text>
                 <text x={x + 10} y="255" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Объём</text>
-                <text x={x + 290} y="255" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.volume}</text>
+                <text x={x + 290} y="255" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.volume}</text>
                 <text x={x + 10} y="275" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Дата</text>
                 <text x={x + 290} y="275" fill="#646478" textAnchor="end" alignmentBaseline="middle">{dateTime.toLocaleDateString()}, {dateTime.toLocaleTimeString()}</text>
             </>
         );
-    }, [isMoving, xClient, candlesticksWithXOffset]);
+    }, [isMoving, xClient, dataWithXOffset]);
 
     const start = useCallback((e: any) => {
         setMoving(true);
--- src/components/line-chart-v2/LineChartV2.tsx
+++ src/components/line-chart-v2/LineChartV2.tsx
@@ -15,46 +15,51 @@ export function LineChartV2(props: ILineChartV2Props) {
     const [xClient, setXClient] = useState(0);
     const [xOffset, setXOffset] = useState(0);
 
-    const { candlesticksWithX, xMin } = useMemo(() => {
-        const candlesticksWithX = [...props.data].reverse().map((i, index) => ({
+    const { dataWithX, xMin } = useMemo(() => {
+        const dataWithX = [...props.data].reverse().map((i, index) => ({
             ...i,
             x: 875 - index * 20,
         })).reverse();
         return {
-            candlesticksWithX,
-            xMin: candlesticksWithX[0].x,
+            dataWithX,
+            xMin: dataWithX.length ? dataWithX[0].x : 0,
         };
     }, [props.data]);
 
-    const { candlesticksWithXOffset, valueMin, valueMax } = useMemo(() => {
-        const candlesticksWithXOffset = candlesticksWithX.map(i => ({
+    const { dataWithXOffset, valueMin, valueMax } = useMemo(() => {
+        const dataWithXOffset = dataWithX.map(i => ({
             ...i,
             x: i.x - xOffset,
         }));
-        const values = candlesticksWithXOffset
+        const values = dataWithXOffset
             .filter(i => i.x >= 125 && i.x <= 875)
             .map(i => i.close);
         return {
-            candlesticksWithXOffset,
+            dataWithXOffset,
             valueMin: Math.min(...values),
             valueMax: Math.max(...values),
         };
-    }, [candlesticksWithX, xOffset]);
+    }, [dataWithX, xOffset]);
 
     const yAxis = useMemo(() => {
-        const yAxis = [];
-        const { close } = candlesticksWithXOffset[candlesticksWithXOffset.length - 1];
+        const yAxis: JSX.Element[] = [];
+
+        if (!dataWithXOffset.length) {
+            return yAxis;
+        }
+
+        const { close } = dataWithXOffset[dataWithXOffset.length - 1];
 
         if (close < valueMin) {
-            yAxis.push(<text key="yAxis-close" x="990" y="425" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
+            yAxis.push(<text key="yAxis-close" x="995" y="425" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
         } else if (close > valueMax) {
-            yAxis.push(<text key="yAxis-close" x="990" y="25" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
+            yAxis.push(<text key="yAxis-close" x="995" y="25" fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>);
         } else {
-            const y = interpolate(close, [valueMin, valueMax], [425, 25]);
+            const y = interpolate(close, [valueMin, valueMax], [425, 25]) || 25;
             yAxis.push(
                 <React.Fragment key="yAxis-close">
                     <line x1="100" y1={y} x2="900" y2={y} stroke="#F26126" strokeDasharray="10 5" />
-                    <text x="990" y={y} fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>
+                    <text x="995" y={y} fill="#F26126" textAnchor="end" alignmentBaseline="middle">{close} &#8381;</text>
                 </React.Fragment>
             );
         }
@@ -62,31 +67,33 @@ export function LineChartV2(props: ILineChartV2Props) {
         const interval = (valueMax - valueMin) / 4;
 
         for (let i = 0; i < 5; i++) {
-            const value = (valueMax - i * interval).toFixed(4);
+            const value = (valueMax - i * interval).toFixed(6);
             const y = i * 100 + 25;
             yAxis.push(
                 <React.Fragment key={`yAxis-${value}`}>
                     <line x1="100" y1={y} x2="900" y2={y} stroke="#F7F7F8" />
-                    <text x="10" y={y} fill="#9393A1" alignmentBaseline="middle">{value} &#8381;</text>
+                    <text x="5" y={y} fill="#9393A1" alignmentBaseline="middle">{value} &#8381;</text>
                 </React.Fragment>
             );
         }
 
         return yAxis;
-    }, [candlesticksWithXOffset, valueMin, valueMax]);
+    }, [dataWithXOffset, valueMin, valueMax]);
 
     const { xAxis, line } = useMemo(() => {
         const xAxis: JSX.Element[] = [];
         const line: JSX.Element[] = [];
         let datePrev = '';
+        let xPrev = dataWithXOffset.length ? dataWithXOffset[0].x : 0;
 
-        candlesticksWithXOffset.forEach((i, index, array) => {
+        dataWithXOffset.forEach((i, index, array) => {
             const date = new Date(i.dateTime).toLocaleDateString();
 
             if (datePrev !== date) {
                 datePrev = date;
 
-                if (i.x >= 125 && i.x <= 875) {
+                if (i.x >= 125 && i.x <= 875 && (i.x - xPrev >= 100 || i.x - xPrev === 0)) {
+                    xPrev = i.x;
                     xAxis.push(
                         <React.Fragment key={`xAxis-${date}`}>
                             <line x1={i.x} y1="0" x2={i.x} y2="450" stroke="#F7F7F8" />
@@ -100,7 +107,7 @@ export function LineChartV2(props: ILineChartV2Props) {
                 const y1 = interpolate(i.close, [valueMin, valueMax], [425, 25]);
                 const y2 = interpolate(array[index + 1].close, [valueMin, valueMax], [425, 25]);
                 const color = array[index + 1].close >= i.close ? '#3CD280' : '#FF5050';
-                line.push(<line x1={i.x} y1={y1} x2={array[index + 1].x} y2={y2} stroke={color} strokeWidth="2" />);
+                line.push(<line key={`line-${i.x}-${array[index + 1].x}`} x1={i.x} y1={y1} x2={array[index + 1].x} y2={y2} stroke={color} strokeWidth="2" />);
             }
         });
 
@@ -108,7 +115,7 @@ export function LineChartV2(props: ILineChartV2Props) {
             xAxis,
             line,
         };
-    }, [candlesticksWithXOffset, valueMin, valueMax]);
+    }, [dataWithXOffset, valueMin, valueMax]);
 
     const tooltip = useMemo(() => {
         if (!ref.current || isMoving || !xClient) {
@@ -117,37 +124,37 @@ export function LineChartV2(props: ILineChartV2Props) {
 
         const boundingClientRect = ref.current.getBoundingClientRect();
         let x = (xClient - boundingClientRect.x) / boundingClientRect.width * 1000;
-        let candlestickHovering: any;
+        let itemHovering: any;
         let intervalPrev = 1000;
-        candlesticksWithXOffset.forEach(i => {
+        dataWithXOffset.forEach(i => {
             const interval = Math.abs(i.x - x);
 
             if (interval <= 10 && interval < intervalPrev) {
                 intervalPrev = interval;
-                candlestickHovering = i;
+                itemHovering = i;
             }
         });
 
-        if (!candlestickHovering || (candlestickHovering.x < 125 || candlestickHovering.x > 875)) {
+        if (!itemHovering || (itemHovering.x < 125 || itemHovering.x > 875)) {
             return null;
         }
 
-        x = candlestickHovering.x >= 500 ? candlestickHovering.x - 320 : candlestickHovering.x + 20;
-        const dateTime = new Date(candlestickHovering.dateTime);
-        const close = interpolate(candlestickHovering.close, [valueMin, valueMax], [425, 25]);
+        x = itemHovering.x >= 500 ? itemHovering.x - 320 : itemHovering.x + 20;
+        const dateTime = new Date(itemHovering.dateTime);
+        const close = interpolate(itemHovering.close, [valueMin, valueMax], [425, 25]);
 
         return (
             <>
-                <line x1={candlestickHovering.x} y1="0" x2={candlestickHovering.x} y2="450" stroke="#9393A1" />
-                <circle cx={candlestickHovering.x} cy={close} r="5" fill="#9393A1" />
+                <line x1={itemHovering.x} y1="0" x2={itemHovering.x} y2="450" stroke="#9393A1" />
+                <circle cx={itemHovering.x} cy={close} r="5" fill="#9393A1" />
                 <rect x={x} y="200" width={300} height="50" fill="#FFFFFF" stroke="#9393A1" />
                 <text x={x + 10} y="215" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Закрытие</text>
-                <text x={x + 290} y="215" fill="#646478" textAnchor="end" alignmentBaseline="middle">{candlestickHovering.close} &#8381;</text>
+                <text x={x + 290} y="215" fill="#646478" textAnchor="end" alignmentBaseline="middle">{itemHovering.close} &#8381;</text>
                 <text x={x + 10} y="235" fill="#646478" alignmentBaseline="middle" fontWeight="bold">Дата</text>
                 <text x={x + 290} y="235" fill="#646478" textAnchor="end" alignmentBaseline="middle">{dateTime.toLocaleDateString()}, {dateTime.toLocaleTimeString()}</text>
             </>
         );
-    }, [isMoving, xClient, candlesticksWithXOffset]);
+    }, [isMoving, xClient, dataWithXOffset]);
 
     const start = useCallback((e: any) => {
         setMoving(true);
